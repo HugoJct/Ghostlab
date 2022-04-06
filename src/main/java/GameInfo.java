97ae94768d331c8f9ac7ace709918b1beb134890
LE FRANC matthieu <matt.lefranc@gmail.com>
2022-04-06 23:35:58 +0200

    gestion des erreurs CommandAskJoin

--- src/main/java/GameInfo.java
+++ src/main/java/GameInfo.java
@@ -4,5 +4,6 @@ import java.util.HashMap;
 
 public class GameInfo {
     public static int nbrGames;
+    public static String id;
     public static HashMap<Integer, Integer> gameIdNbrPlayers = new HashMap<>();
 }
--- src/main/java/commands/out/CommandAskJoin.java
+++ src/main/java/commands/out/CommandAskJoin.java
@@ -2,9 +2,12 @@ package main.java.commands.out;
 
 import java.io.PrintWriter;
 
+import main.java.GameInfo;
 import main.java.client.Client;
 import main.java.client.ClientTCP;
 import main.java.commands.Command;
+import main.java.console.DebugLogger;
+import main.java.console.DebugType;
 
 // REGIS id port m***
 
@@ -17,6 +20,15 @@ public class CommandAskJoin extends Command {
     @Override
     public void execute(ClientTCP client, String[] args) {
 
+        if (args.length < 4) {
+            DebugLogger.print(DebugType.ERROR, "[ATTENTION] Tout les paramètres de la commande ne sont pas renseignés. Rappel : REGIS id port m");
+            return;
+        }
+
+        if (!GameInfo.gameIdNbrPlayers.containsKey(Integer.parseInt(args[3]))) {
+            DebugLogger.print(DebugType.ERROR, "[ATTENTION] La partir donnée n'existe pas");
+            return;
+        }
     }
     
 }
