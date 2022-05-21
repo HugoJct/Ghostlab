10516daf7e0da7b620ecdd5d5b9969db652236dd
Hugo Jacotot <hjacotot@free.fr>
2022-05-21 17:14:46 +0200

    added conditions in move commands

--- src/main/java/commands/out/tcp/CommandAskTcpMoveRight.java
+++ src/main/java/commands/out/tcp/CommandAskTcpMoveRight.java
@@ -27,6 +27,11 @@ public class CommandAskTcpMoveRight extends CommandTCP {
     @Override
     public void execute(ClientTCP clientTCP, String[] args) {
         DebugLogger.print(DebugType.CONFIRM, "COMMAND : DOMOV");
+
+        if (!GameInfo.hasGameStarted) {
+            DebugLogger.print(DebugType.ERROR, "[CommandAskTcpDropGame/WARNING] : le jeu n'a pas encore commencé, cette commande sera ignorée");
+            return;
+        }
         
         if (args.length < 2) {
             DebugLogger.print(DebugType.WARNING, "[CommandAskTcpMoveDown/WARNING] : nombre d'arguments minimum non attend, (RAPPEL : DOMOV d), cette commande sera ignorée");
